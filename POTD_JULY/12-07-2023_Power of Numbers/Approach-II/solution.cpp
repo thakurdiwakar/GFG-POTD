68b9d16f7c78ec9925b8cba9c1c84e84af87fd2f
diwakarsingh <ds6228353@gmail.com>
2023-07-12 11:37:57 +0530

    Added Power Of Numbers

--- POTD_JULY/12-07-2023_Power of Numbers/Approach-II/solution.cpp
+++ POTD_JULY/12-07-2023_Power of Numbers/Approach-II/solution.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:
-   
+
 
     long long power(int N, int R)
     {
@@ -10,17 +10,13 @@ public:
         {
             return N % 1000000007;  // Return N modulo 1000000007
         }
-        
         // Divide the problem into smaller subproblems
         long long result = power(N, R/2);  // Calculate power(N, R/2)
-        
         result = (result * result) % 1000000007;  // Multiply the result with itself and take modulo
-        
         if(R % 2 == 1)
         {
             result = (result * N) % 1000000007;  // If R is odd, multiply result with N and take modulo
         }
-        
         return result;  // Return the final result
     }
 };
