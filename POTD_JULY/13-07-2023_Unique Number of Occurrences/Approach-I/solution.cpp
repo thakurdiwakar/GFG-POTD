271780e11bb47932e6d613a85667b889047428c7
Diwakar Singh <76618190+thakurdiwakar@users.noreply.github.com>
2023-07-13 06:56:40 +0530

    Added Unique Number of Occurrences

--- POTD_JULY/13-07-2023_Unique Number of Occurrences/Approach-I/solution.cpp
+++ POTD_JULY/13-07-2023_Unique Number of Occurrences/Approach-I/solution.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 //User function Template for C++
 class Solution
 {
@@ -11,8 +5,6 @@ class Solution
     bool isFrequencyUnique(int n, int arr[])
 {
     unordered_map<int, int> frequencyMap, frequencyCount;
-
-  
     for(int i = 0; i < n; i++)
         frequencyMap[arr[i]]++;
 
@@ -26,32 +18,7 @@ class Solution
         // Increment the count 
         frequencyCount[x.second]++;
     }
-
    //for unique
     return true;
 }
-
-
 };
-
-//{ Driver Code Starts.
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        Solution ob;
-        bool ans=ob.isFrequencyUnique(n,arr);
-        if(ans)
-            cout<<1<<endl;
-        else
-            cout<<0<<endl;
-    }
-}
-// } Driver Code Ends
